008f8b71c1ca790ba5f9cea3535e1eaa6fec8e7d
Mohd Ajmal Raza <mohdajmalraza92786@gmail.com>
2026-08-10 02:12:13 +0530

    feat(auth): add guest route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,13 +2,26 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import Chat from "../pages/Chat";
+import ProtectedRoute from "./ProtectedRoute";
+import HomeRedirect from "./HomeRedirect";
+import GuestRoute from "./GuestRoute";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/chat" element={<Chat />} />
+      {/* Root */}
+      <Route path="/" element={<HomeRedirect />} />
+
+      {/* Guest-only routes */}
+      <Route element={<GuestRoute />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
+
+      {/* Protected routes */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/chat" element={<Chat />} />
+      </Route>
     </Routes>
   );
 };
